90f8b6b8be30f367b5e50b0505d70a5534e6cea3
Kreso Maric <kresomaric66@gmail.com>
2019-03-15 13:28:09 +0000

    New commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var scoreNumber = document.getElementById("scoreNumber");
+var p1Button = document.getElementById("p1");
+var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var winningScoreDisplay = document.querySelector("p span");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+p1Button.addEventListener("click", function(){
+	if(!gameOver){
+		p1Score++;
+	} if(p1Score === winningScore){
+		gameOver = true;
+		p1Display.classList.add("winner");
+	}
+	p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+	if(!gameOver){
+		p2Score++;
+	} if(p2Score === winningScore){
+		gameOver = true;
+		p2Display.classList.add("winner");
+	}
+	p2Display.textContent = p2Score;
+});
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
+	p1Score= 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+scoreNumber.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
